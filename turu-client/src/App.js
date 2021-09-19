b79ed553be58d391e4a40fd41548eb8f1f76c711
Imron Reviady <imronreviady2@gmail.com>
2021-09-20 06:17:46 +0700

    route condition by user login

--- turu-client/src/App.js
+++ turu-client/src/App.js
@@ -3,8 +3,42 @@ import Login from "./pages/Login/Login";
 import Profile from "./pages/Profile/Profile";
 import Register from "./pages/Register/Register";
 
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
 function App() {
-	return <Register />;
+	const { user } = useContext(AuthContext);
+
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					{user ? <Home /> : <Register />}
+				</Route>
+				<Route path="/profile/:username" component={Profile} />
+				<Route path="/login">
+					{user ? (
+						<Redirect to={`/profile/${user.username}`} />
+					) : (
+						<Login />
+					)}
+				</Route>
+				<Route path="/register">
+					{user ? (
+						<Redirect to={`/profile/${user.username}`} />
+					) : (
+						<Register />
+					)}
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
