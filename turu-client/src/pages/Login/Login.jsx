cea84ad6913e76352dd38e4a6d2335965e951c28
Imron Reviady <imronreviady2@gmail.com>
2021-09-20 06:20:48 +0700

    add login functionality

--- turu-client/src/pages/Login/Login.jsx
+++ turu-client/src/pages/Login/Login.jsx
@@ -1,6 +1,20 @@
 import './login.css'
+import { login } from '../../api'
+import { useContext } from 'react'
+import { AuthContext } from '../../context/AuthContext'
+import { CircularProgress } from '@material-ui/core'
 
 export default function Login() {
+    const { user, isFatching, error, dispatch } = useContext(AuthContext)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const email = e.target.email.value
+        const password = e.target.password.value
+        
+        login({ email, password }, dispatch)
+    }
+
     return (
         <div className="login">
             <div className="loginWrapper">
@@ -9,13 +23,13 @@ export default function Login() {
                     <span className="loginDesc">Connect with people and world around you!</span>
                 </div>
                 <div className="loginRight">
-                    <div className="loginBox">
-                        <input placeholder="Email" className="loginInput" />
-                        <input placeholder="Password" className="loginInput" />
-                        <button className="loginButton">Log In</button>
+                    <form className="loginBox" onSubmit={handleSubmit}>
+                        <input placeholder="Email" required type="email" name="email" className="loginInput" />
+                        <input placeholder="Password" minLength="5" required type="password" name="password" className="loginInput" />
+                        <button type="submit" className="loginButton" disabled={isFatching}>{ isFatching ? <CircularProgress color="white" /> : "Log In" }</button>
                         <span className="loginForgot">Forgot Password?</span>
-                        <button className="loginRegisterButton">Create a new account</button>
-                    </div>
+                        <button className="loginRegisterButton">{ isFatching ? <CircularProgress color="white" /> : "Create a new account" }</button>
+                    </form>
                 </div>
             </div>
         </div>
