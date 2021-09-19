2383d33a57788c6c53bddbe955d3c75b8bcb0d9c
Imron Reviady <imronreviady2@gmail.com>
2021-09-20 06:14:46 +0700

    Register functionality

--- turu-client/src/pages/Register/Register.jsx
+++ turu-client/src/pages/Register/Register.jsx
@@ -1,6 +1,37 @@
+import axios from 'axios'
+import { useHistory } from 'react-router'
 import './register.css'
 
 export default function Register() {
+    const history = useHistory()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const username = e.target.username.value
+        const email = e.target.email.value
+        const password = e.target.password.value
+        const passwordConfirm = e.target.passwordConfirm.value
+
+        if (passwordConfirm !== password) {
+            alert('Password confirmation does not match')
+        } else {
+            const user = {
+                username,
+                email,
+                password
+            }
+
+            try {
+                await axios.post('/api/auth/register', user)
+                history.push('/login')
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        
+        //login({ email, password }, dispatch)
+    }
+
     return (
         <div className="login">
             <div className="loginWrapper">
@@ -9,14 +40,14 @@ export default function Register() {
                     <span className="loginDesc">Connect with people and world around you!</span>
                 </div>
                 <div className="loginRight">
-                    <div className="loginBox">
-                        <input placeholder="Username" className="loginInput" />
-                        <input placeholder="Email" className="loginInput" />
-                        <input placeholder="Password" className="loginInput" />
-                        <input placeholder="Confirm Password" className="loginInput" />
-                        <button className="loginButton">Sign Up</button>
+                    <form className="loginBox" onSubmit={handleSubmit}>
+                        <input placeholder="Username" type="text" name="username" className="loginInput" required />
+                        <input placeholder="Email" type="email" name="email" className="loginInput" required />
+                        <input placeholder="Password" type="password" name="password" className="loginInput" required />
+                        <input placeholder="Confirm Password" type="password" name="passwordConfirm" className="loginInput" required />
+                        <button className="loginButton" type="submit">Sign Up</button>
                         <button className="loginRegisterButton">Login to your account</button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
