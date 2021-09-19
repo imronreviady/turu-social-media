6e4700ea5e4f278be808479e325def3e1c525f69
Imron Reviady <imronreviady2@gmail.com>
2021-09-20 06:15:19 +0700

    get timeline posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -71,6 +71,23 @@ router.get("/:id", async (req, res) => {
 	}
 });
 
+// get timeline posts
+
+router.get("/timeline/all", async (req, res) => {
+	try {
+		const currentUser = await User.findById(req.body.userId);
+		const userPosts = await Post.find({ userId: currentUser._id });
+		const friendPosts = await Promise.all(
+			currentUser.followings.map((friendId) => {
+				return Post.find({ userId: friendId });
+			})
+		);
+		res.status(200).json(userPosts.concat(...friendPosts));
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
 // get a timeline posts
 router.get("/timeline/:userId", async (req, res) => {
 	try {
