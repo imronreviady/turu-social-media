19f8307d54ec7d77be903de0bbda6d275faa0f94
Imron Reviady <imronreviady2@gmail.com>
2021-09-20 06:18:55 +0700

    get real data from API

--- turu-client/src/components/Feed/Feed.jsx
+++ turu-client/src/components/Feed/Feed.jsx
@@ -1,14 +1,26 @@
+import { useState, useEffect } from 'react'
 import Post from '../Post/Post'
 import Share from '../Share/Share'
 import './feed.css'
-import { Posts } from '../../dummyData'
+// import { Posts } from '../../dummyData'
+import axios from 'axios'
 
-export default function Feed() {
+export default function Feed({ username }) {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const fatchPosts = async () => {
+            const res = username ? await axios.get(`/api/posts/profile/${username}`) : await axios.get('/api/posts/timeline/6144a3325f7dc88a0b242659')
+            setPosts(res.data)
+        }
+        fatchPosts()
+    }, [username]);
+    
     return (
         <div className="feed">
             <div className="feedWrapper">
                 <Share />
-                {Posts.map((post) => (<Post key={post.id} post={post} />))}
+                {posts.map((post) => (<Post key={post._id} post={post} />))}
             </div>
         </div>
     )
--- turu-client/src/components/Post/Post.jsx
+++ turu-client/src/components/Post/Post.jsx
@@ -1,11 +1,23 @@
+import { Link } from 'react-router-dom'
 import { MoreVert } from '@material-ui/icons'
 import './post.css'
-import { Users } from '../../dummyData'
-import { useState } from 'react'
+//import { Users } from '../../dummyData'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { format } from 'timeago.js'
 
 export default function Post({ post }) {
-    const [like, setLike] = useState(post.like)
+    const [like, setLike] = useState(post.likes.length)
     const [isLiked, setIsLiked] = useState(false)
+    const [user, setUser] = useState({})
+
+    useEffect(() => {
+        const fatchUser = async () => {
+            const res = await axios.get(`/api/users/?userId=${post.userId}`)
+            setUser(res.data)
+        }
+        fatchUser()
+    }, [post.userId]);
 
     const likeHandler = () => {
         setLike(isLiked ? like - 1 : like + 1)
@@ -17,9 +29,13 @@ export default function Post({ post }) {
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img src={`/assets/${Users.filter(u => u.id === post.userId)[0].profilePicture}`} alt="" className="postProfileImg" />
-                        <span className="postUsername">{Users.filter(u => u.id === post.userId)[0].username}</span>
-                        <span className="postDate">{ post.date }</span>
+                        <Link to={`/profile/${user.username}`}>
+                            <img src={`/assets/${user.profilePicture || "person/noAvatar.png"}`} alt="" className="postProfileImg" />
+                        </Link>
+                        <Link to={`/profile/${user.username}`} style={{ textDecoration: "none" }}>
+                            <span className="postUsername">{user.username}</span>
+                        </Link>
+                        <span className="postDate">{ format(post.createdAt) }</span>
                     </div>
                     <div className="postTopRight">
                         <MoreVert />
@@ -27,7 +43,7 @@ export default function Post({ post }) {
                 </div>
                 <div className="postCenter">
                     <span className="postText">{ post?.desc }</span>
-                    <img src={`/assets/${post?.photo}`} alt="" className="postImg" />
+                    <img src={`/assets/${post?.img}`} alt="" className="postImg" />
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
