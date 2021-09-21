ecd2d4d69a8c1308adab213795cd45a880c0bb0c
Imron Reviady <imronreviady2@gmail.com>
2021-09-22 00:30:58 +0700

    sort post by createdAt

--- turu-client/src/components/Feed/Feed.jsx
+++ turu-client/src/components/Feed/Feed.jsx
@@ -1,32 +1,39 @@
-import { useState, useEffect, useContext } from 'react'
-import Post from '../Post/Post'
-import Share from '../Share/Share'
-import './feed.css'
+import { useState, useEffect, useContext } from "react";
+import Post from "../Post/Post";
+import Share from "../Share/Share";
+import "./feed.css";
 // import { Posts } from '../../dummyData'
-import axios from 'axios'
-import { AuthContext } from '../../context/AuthContext'
+import axios from "axios";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Feed({ username }) {
-    const [posts, setPosts] = useState([])
-    const { user } = useContext(AuthContext)
+	const [posts, setPosts] = useState([]);
+	const { user } = useContext(AuthContext);
 
-    useEffect(() => {
-        const fatchPosts = async () => {
-            const res = username ?
-                await axios.get(`/api/posts/profile/${username}`)
-                :
-                await axios.get(`/api/posts/timeline/${user._id}`)
-            setPosts(res.data)
-        }
-        fatchPosts()
-    }, [username, user._id]);
-    
-    return (
-        <div className="feed">
-            <div className="feedWrapper">
-                <Share />
-                {posts.map((post) => (<Post key={post._id} post={post} />))}
-            </div>
-        </div>
-    )
+	useEffect(() => {
+		const fatchPosts = async () => {
+			const res = username
+				? await axios.get(`/api/posts/profile/${username}`)
+				: await axios.get(`/api/posts/timeline/${user._id}`);
+			setPosts(
+				res.data.sort((post1, post2) => {
+					return (
+						new Date(post2.createdAt) - new Date(post1.createdAt)
+					);
+				})
+			);
+		};
+		fatchPosts();
+	}, [username, user._id]);
+
+	return (
+		<div className="feed">
+			<div className="feedWrapper">
+				{(!username || username === user.username) && <Share />}
+				{posts.map((post) => (
+					<Post key={post._id} post={post} />
+				))}
+			</div>
+		</div>
+	);
 }
