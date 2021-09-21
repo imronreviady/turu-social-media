78e8bf8f9c467a14d21951e83ffd335256011158
Imron Reviady <imronreviady2@gmail.com>
2021-09-21 07:48:54 +0700

    fix bug image upload

--- api/index.js
+++ api/index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
-
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const multer = require("multer");
-
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
-
 const router = express.Router();
 const path = require("path");
 
@@ -18,15 +15,11 @@ dotenv.config();
 
 mongoose.connect(
 	process.env.MONGO_URL,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	},
+	{ useNewUrlParser: true, useUnifiedTopology: true },
 	() => {
-		console.log("Connected to MongoDb");
+		console.log("Connected to MongoDB");
 	}
 );
-
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 //middleware
@@ -46,17 +39,16 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 app.post("/api/upload", upload.single("file"), (req, res) => {
 	try {
-		return res.status(200).json("File upload succesfully");
+		return res.status(200).json("File uploded successfully");
 	} catch (error) {
 		console.error(error);
 	}
 });
 
-// routes
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(process.env.PORT || 8800, () => {
-	console.log("Backend Server started");
+app.listen(8800, () => {
+	console.log("Backend server is running!");
 });
